00d8061b96d38339a1f7e43f4482c35f0db77f90
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-04-26 11:12:41 +0300

    cv2 prac
    learn how to connect to phones camera with ip

---
+++ cv2.py
@@ -0,0 +1,20 @@
+#before running the program check out if u download a camera by ip and dont forget to change url if its different
+
+import requests
+import cv2
+import numpy as np	
+import imutils
+
+url = "http://192.168.60.101:8080/shot.jpg"
+
+while True:
+	img_resp = requests.get(url)
+	img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+	img = cv2.imdecode(img_arr, -1)
+	img = imutils.resize(img, width=1000, height=1800)
+	cv2.imshow("Android_cam", img)
+
+	if cv2.waitKey(1) == 27:
+		break
+
+cv2.destroyAllWindows()
