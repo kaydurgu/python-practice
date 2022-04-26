017f95c4fc8a15e6a9cd2f4da9fcd57b9900f4a1
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-04-26 11:29:06 +0300

    changed resolution of camera

--- cv2.py
+++ cv2.py
@@ -11,7 +11,9 @@ while True:
 	img_resp = requests.get(url)
 	img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
 	img = cv2.imdecode(img_arr, -1)
-	img = imutils.resize(img, width=1000, height=1800)
+
+	img = imutils.resize(img, width=1800, height=2500)
+
 	cv2.imshow("Android_cam", img)
 
 	if cv2.waitKey(1) == 27:
