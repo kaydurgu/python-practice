11af4a32c73f487c8384b65ca2743dde9ac02eef
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-04-30 14:16:47 +0300

    Add files via upload

---
+++ assert_01.py
@@ -0,0 +1,32 @@
+def div(a: int , b: int ) -> int:
+    assert b!=0, 'Нельзя делить на ноль'  # if assert con -> True Нельзя делить на ноль 
+    return int(a / b)
+div (1, 0)
+
+'''
+Метод                       Проверка на 
+
+assertEqual(x, y)	        x == y	
+
+assertNotEqual(x, y)	    x != y	
+
+assertTrue(x)	            bool(x) равно True	
+
+assertFalse(x)	            bool(x) равно False	
+
+assertIs(x, y)	            x это y	
+
+assertIsNot(x, y)	        x это не y	
+
+assertIsNone(x)	            x это None	
+
+assertIsNotNone(x)	        x это не None	
+
+assertIn(x, y)	            x в y	
+
+assertNotIn(x, y)	        x не в y	
+
+assertIsInstance(x, y)	    isinstance(x, y)	
+
+assertNotIsInstance(x,y)	не isinstance(x, y)
+'''
