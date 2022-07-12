4b5588d28aba72849710a9732a6299ba36dd1722
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-07-12 10:27:24 +0300

    descriptor3
    descriptor 3

---
+++ Miscellanea/descriptors2.py
@@ -0,0 +1,32 @@
+class ValidateString:
+    def __init__(self,min_length = 3, max_length = 100):
+        self.min_length = min_length
+        self.max_length = max_length
+    def validate(self, string):
+        return type(string) == str and self.min_length <= len(string) <= self.max_length
+    
+class StringValue:
+    def __init__(self, validator = ValidateString()):
+        self.validator = validator
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+    def __get__(self,instance, owner):
+        return instance.__dict__[self.name]
+    def __set__(self, instance,value):
+        if self.validator.validate(value):
+            instance.__dict__[self.name] = value
+     
+class RegisterForm:
+    login = StringValue()
+    password = StringValue()
+    email = StringValue()
+    def __init__(self,login ,password, email):
+        self.login = login
+        self.password = password
+        self.email = email
+    def get_fields(self):
+        return [self.login, self.password, self.email]
+    def show(self):
+        print (f'<form>\nЛогин: {self.login}\nПароль: {self.password}\nEmail: {self.email}\n</form>')
+rf = RegisterForm('Bo00', 'FooBar', 'BuzFuz')
+rf.show()
