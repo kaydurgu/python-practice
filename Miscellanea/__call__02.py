abe4e67078f069eba69e453f5de861338c0892c8
kaydurgu <57888663+kaydurgu@users.noreply.github.com>
2022-07-25 08:50:21 +0300

    __call__ 02
    __call__02

---
+++ Miscellanea/__call__02.py
@@ -0,0 +1,9 @@
+class DigitRetrieve:
+    def __call__(self, *args, **kwargs):
+        try:
+            return int(args[0])
+        except ValueError:
+            return None
+dg = DigitRetrieve()   
+st = ["123", "abc", "-56.4", "0", "-5"]
+digits = list(map(dg, st))  # [123, None, None, 0, -5]
