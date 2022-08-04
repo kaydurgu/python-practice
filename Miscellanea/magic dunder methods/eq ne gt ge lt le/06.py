9310c30edddae1b54e5c92daf471067c5c365300
kaydurgu <kaydurgu@gmail.com>
2022-08-04 10:43:48 +0300

    using endswith and tuple-list opening

--- Miscellanea/magic dunder methods/eq ne gt ge lt le/06.py
+++ Miscellanea/magic dunder methods/eq ne gt ge lt le/06.py
@@ -1,2 +1,17 @@
+class FileAcceptor:
+    def __init__(self, *exts) -> None:
+        self.exts = exts
+    
+    def __call__(self, smth : str, *args, **kwds):
+        return smth.endswith(self.exts)
 
-#dunder
+    def __add__(self, other):
+        return FileAcceptor(*self.exts, *other.exts)
+
+filenames = ["boat.jpg", "ans.web.png", "text.txt", "www.python.doc", "my.ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.xls"]
+acceptor1 = FileAcceptor("jpg", "jpeg", "png")
+acceptor2 = FileAcceptor("png", "bmp")
+acceptor12 = acceptor1 + acceptor2    # ("jpg", "jpeg", "png", "bmp")
+acceptor_images = FileAcceptor("jpg", "jpeg", "png")
+acceptor_docs = FileAcceptor("txt", "doc")
+filenames = list(filter(acceptor_images + acceptor_docs, filenames))
